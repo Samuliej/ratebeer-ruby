654d96c1a714541c8c03b43e1a3aa93b44bfc1ed
Samuli <samuli.toppi@outlook.com>
2025-02-18 10:38:11 +0200

    Tehtävä 3 valmis

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,26 @@
 class Brewery < ApplicationRecord
   has_many :beers
+
+  def print_report
+    puts name
+    puts "established at year #{year}"
+    puts "number of beers #{beers.count}"
+  end
+
+  def restart
+    # Kutsuu olion omaa year metodia
+    self.year = 2022
+    puts "changed year to #{year}"
+  end
+
+
+  # Defaultit getterit ja setterit näyttävät
+  # suurinpiirtein tältä
+  # def year
+  #   read_attribute(:year)
+  # end
+  #
+  # def year=(value)
+  #   write_attribute(:year, value)
+  # end
 end
