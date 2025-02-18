6641a588d169fd4f2cd1bd9ee17247cca76df454
Samuli <samuli.toppi@outlook.com>
2025-02-18 11:36:18 +0200

    Tehtävä 5 valmis, toteutettu käyttäen map ja sum, sillä tehtävässä 4 käytin jo inject

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,9 +5,15 @@ class Beer < ApplicationRecord
   def average_rating
     # Lasketaan oluet olion ratings olioiden
     # scorejen summa
-    summa = self.ratings.inject(0) {
-      |sum, rating| sum + rating.score
-    }
+    # Toteutin tehtävä 4:n reducella, eli
+    # injectillä, joten tehdään
+    # tehtävä 5 mapilla:
+
+    # Tekee taulukon pelkistä scoreista
+    scores = self.ratings.map { |rating| rating.score }
+
+    # Lasketaan scorejen summa
+    summa = scores.sum
 
     # Lasketaan keskiarvo sekä tiivistetään yhteen desimaaliin
     (summa / (self.ratings.count.to_f)).truncate(1)
