d244c7a829d207216f4f4ea3a3334ce6aecb43d6
Samuli <samuli.toppi@outlook.com>
2025-02-18 10:45:11 +0200

    Tehtävä 3 muokattu käyttämään olion omaa to_s metodia

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,11 @@
 class Rating < ApplicationRecord
   belongs_to :beer
+
+  def print
+    puts "#{self.beer.name} #{self.score}"
+  end
+
+  def to_s
+    "#{self.beer.name} #{self.score}"
+  end
 end
