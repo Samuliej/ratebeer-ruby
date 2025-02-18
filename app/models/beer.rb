f756d649f5aa2dac5b0a6450f886ab530bba7610
Samuli <samuli.toppi@outlook.com>
2025-02-18 11:24:58 +0200

    Tehtävä 4 valmis

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,15 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    # Lasketaan oluet olion ratings olioiden
+    # scorejen summa
+    summa = self.ratings.inject(0) {
+      |sum, rating| sum + rating.score
+    }
+
+    # Lasketaan keskiarvo sekä tiivistetään yhteen desimaaliin
+    (summa / (self.ratings.count.to_f)).truncate(1)
+  end
 end
