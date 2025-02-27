a307041935b4e792baed18d5506d034781d1f0ff
Samuli <samuli.toppi@outlook.com>
2025-02-27 19:18:33 +0200

    Viikko 3, tehtävä 4 valmis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Määritellään, että metodi current_user tulee käyttöön myös näkymissä
+  helper_method :current_user
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
@@ -7,5 +9,11 @@ class ApplicationController < ActionController::Base
   def switch_locale(&)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &)
+  end
+
+  def current_user
+    return nil if session[:user_id].nil?
+
+    User.find(session[:user_id])
   end
 end
