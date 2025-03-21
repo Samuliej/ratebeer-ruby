c009c6b22c941d4018f8b7d11b933ff1f0701cb3
Samuli Toppi <samuli.toppi@outlook.com>
2025-03-21 15:00:33 +0200

    Viikko 4, tehtävä 7 valmis

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -6,11 +6,13 @@ describe "Rating Page" do
   let!(:brewery) { FactoryBot.create :brewery, name: "Koff" }
   let!(:beer1) { FactoryBot.create :beer, name: "Iso 3", brewery: brewery }
   let!(:beer2) { FactoryBot.create :beer, name: "Karhu", brewery: brewery }
+  let!(:beer3) { FactoryBot.create :beer, name: "Kalia", brewery: brewery }
   let!(:user) { FactoryBot.create :user }
+  let!(:user2) { FactoryBot.create :user, username: "EiPekka"}
 
   let!(:rating1) { FactoryBot.create :rating, beer: beer1, user: user }
   let!(:rating2) { FactoryBot.create :rating, beer: beer2, user: user }
-  AMOUNT_OF_RATINGS = 2
+  let!(:rating3) { FactoryBot.create :rating, beer: beer3, user: user2 }
 
   before :each do
     sign_in(username: "Pekka", password: "F00bar%")
@@ -34,7 +36,21 @@ describe "Rating Page" do
     visit ratings_path
     expect(page).to have_content "List of ratings"
     expect(page).to have_content "Ratings count: #{Rating.count}"
-    expect(page).to have_content "Iso 3"
+    expect(page).to have_content beer1.name
+    expect(page).to have_content beer2.name
+    expect(page).to have_content beer3.name
     expect(page).to have_content "10"
+  end
+
+  it "visiting user's page shows the current user's created ratings and not any other ratings" do
+    visit user_path(user)
+
+    expect(page).to have_content user.username
+    expect(page).to have_content "Has made #{Rating.count-1} ratings"
+    expect(page).to have_content "Ratings"
+    expect(page).to have_content beer1.name
+    expect(page).to have_content beer2.name
+
+    expect(page).not_to have_content beer3.name
   end
 end
