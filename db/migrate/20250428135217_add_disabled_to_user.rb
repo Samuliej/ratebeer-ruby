70c28a54ff539f21ab10088bf2c6b0ae73ca5163
Samuli Toppi <samuli.toppi@outlook.com>
2025-04-28 17:38:16 +0300

    Lisätty vakioarvot admin ja disabled kentälle. Korjattu parit bugit

--- db/migrate/20250428135217_add_disabled_to_user.rb
+++ db/migrate/20250428135217_add_disabled_to_user.rb
@@ -1,5 +1,5 @@
 class AddDisabledToUser < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :disabled, :boolean
+    add_column :users, :disabled, :boolean, default: false
   end
 end
